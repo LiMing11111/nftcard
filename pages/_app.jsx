d78c10aeaf96ff7c3c7e21110db36936c25b07d0
“crislee” <505267309@qq.com>
2022-08-18 17:59:04 +0800

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-08-18 01:14:03
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-08-18 17:16:52
+ * @FilePath: /nameoftheproject/pages/_app.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import '../styles/globals.css'
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+export default MyApp
---
+++ pages/nftCard.jsx
@@ -0,0 +1,30 @@
+/*
+ * @Author: error: git config user.name && git config user.email & please set dead value or install git
+ * @Date: 2022-08-18 17:39:55
+ * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
+ * @LastEditTime: 2022-08-18 17:40:08
+ * @FilePath: /nameoftheproject/pages/nftCard.jsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+export const NFTCard = ({ nft }) => {
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">Id: {nft.id.tokenId}</p>
+                <p className="text-gray-600" >{nft.contract.address}</p>
+            </div>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{nft.description}</p>
+            </div>
+        </div>
+
+    </div>
+    )
+}
